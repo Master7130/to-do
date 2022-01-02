73435e7c52efe6f79b3ed5e509cbdbbc567ab84c
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-01-02 08:53:29 -0600

    src/App.css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import './App.css';
 import List from './components/List';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  // Adds lists
   const [lists, setLists] = useState([]);
 
   const click = event => {
     setLists(lists.concat(<List key={lists.length} />));
   };
+  // End
+
+  // Function to get the current date
+  function getDate() {
+    var currentDate = new Date();
+    return currentDate.getMonth() + "/" + currentDate.getDate();
+  }
+
+
+  // Updates time
+  var currentDate = new Date();
+  const [time, setTime] = useState(currentDate.getHours() + ":" + currentDate.getMinutes());
+
+  useEffect(() => {
+    setInterval(() => {
+      var currentDate = new Date();
+      setTime(currentDate.getHours() + ":" + currentDate.getMinutes());
+    }, 1000);
+  }, []);
+  // End
 
   return (
     <div className="App">
       <div id="heading">
-        <h2>Date</h2>
-        <h2>Time</h2>
+        <h2>{getDate()}</h2>
+        <h2>{time}</h2>
       </div>
 
       <button onClick={click} className='add-list'>Add List</button>
-      {lists}
+
+      <div className='lists'>
+        {lists}
+      </div>
     </div>
   );
 }
