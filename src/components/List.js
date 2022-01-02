5c8fa5ef4b235497a47d70e9c889dbcd04042501
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-01-02 08:54:58 -0600

    changes

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,24 @@
 import './List.css'
 
 export default function List(props) {
-    return(
+    return (
         <div className="list-container">
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
+            <p>List</p>
             <p>List</p>
         </div>
     )
