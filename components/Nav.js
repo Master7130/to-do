44b670d324378daeb629c0412099194464bca072
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-05-20 09:23:57 -0500

    CHECKPOINT

---
+++ components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Nav() {
+    return (
+        <div>
+            <Link href='/login'><a>Log in</a></Link>
+        </div>
+    )
+}
