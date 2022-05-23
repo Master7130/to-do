6a8e97c65d262fabcb25eb9a934ed040b1141a0d
Bhavesh Mandalapu <mandalapubhavesh@gmail.com>
2022-05-23 10:14:26 -0500

    changes

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from './nav.module.css'
 
 export default function Nav() {
     return (
-        <div>
-            <Link href='/login'><a>Log in</a></Link>
+        <div className={styles.nav_container}>
+            <Link href="/"><a className={styles.title}>To-do</a></Link>
+
+            <div className={styles.nav_content}>
+                <Link href='/login'><a className={styles.nav_item}>Log In</a></Link>
+                <Link href='/login'><a className={styles.nav_item}>Sign Up</a></Link>
+            </div>
         </div>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import Nav from '../components/Nav'
+import { SessionProvider } from "next-auth/react"
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
+    <SessionProvider session={session} refetchInterval={5 * 60} >
       <Nav />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   )
 }
 
